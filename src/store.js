f4918682fc5e8433ba395849a4871f5b7cadaec9
pgu <pguilcher@restlet.com>
2018-06-11 16:37:27 +0200

    wip

--- src/store.js
+++ src/store.js
@@ -3,7 +3,6 @@ import { routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
 import rootReducer from './modules'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
 export const history = createHistory()
 
@@ -13,14 +12,6 @@ const middleware = [
   thunk,
   routerMiddleware(history)
 ]
-
-if (process.env.NODE_ENV === 'development') {
-  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__
-
-  if (typeof devToolsExtension === 'function') {
-    enhancers.push(devToolsExtension())
-  }
-}
 
 const composedEnhancers = compose(
   applyMiddleware(...middleware),
